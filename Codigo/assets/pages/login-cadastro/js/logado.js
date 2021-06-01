4e685ae1a130519961b6545b14026482ee3b76d3
Breno1210 <brenorosaalmeida@yahoo.com.br>
2021-05-31 22:34:23 -0300

    Login Quase Funcionando

---
+++ Codigo/assets/pages/login-cadastro/js/logado.js
@@ -0,0 +1,11 @@
+/*Função que irá verificar se o usuário tem Acesso, se */
+/*var logado = false;
+
+if(localStorage.getItem("acesso") == "true"){
+    logado = true;
+};
+
+if(logado != true){
+    alert("Você não esta autenticado!");
+    window.location.href="cadastro.html";
+}*/
