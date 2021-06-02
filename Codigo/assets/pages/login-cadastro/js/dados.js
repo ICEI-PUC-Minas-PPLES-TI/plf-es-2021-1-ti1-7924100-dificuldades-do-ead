ee41c58db385554fd5d15808713a22d6f3e0e04a
andre-brandao <brandaoandre00@gmail.com>
2021-06-02 20:21:51 -0300

    js para recuperacao de senha

--- Codigo/assets/pages/login-cadastro/js/dados.js
+++ Codigo/assets/pages/login-cadastro/js/dados.js
@@ -84,3 +84,34 @@ function loginDados() {
 
 
 
+
+//[---------------RECUPERAR SENHA-------------]
+
+function trocarSenha() {
+
+
+    var Email = document.querySelector("input#Email").value;
+    var Resposta = document.querySelector("input#Resposta").value;
+    //var Senha = document.querySelector("input#Senha").value;
+
+
+    for (let i = 0; i < db.usuarios.length; i++) {//ler usuario do banco de dados
+        var usuario = db.usuarios[i];
+
+        if (Email == usuario.Email && Resposta == usuario.Resposta) {//identificar se a resposta está correta
+
+            let strSenha = document.getElementById('campoSenha').value;
+
+            db.usuario[i].Senha = strSenha
+
+
+            window.alert("Senha alterada com sucesso");
+
+            window.location.href = "#";//colocar link para pagina de login
+
+            salvaDados(db)
+        }
+    }
+}
+
+//[------------------------FIM RECUPERAR SENHA-----------]
