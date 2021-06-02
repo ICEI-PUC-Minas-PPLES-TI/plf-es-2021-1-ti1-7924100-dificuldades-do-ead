a8d6cd5610de0781ad3df8be47d784e797964286
Gabriel Victor <gabriel.victorc13@gmail.com>
2021-06-02 00:22:51 -0300

    Apagado o antigo banco de dados

--- Codigo/assets/pages/login-cadastro/js/dados.js
+++ Codigo/assets/pages/login-cadastro/js/dados.js
@@ -1,12 +1,4 @@
-//Esee é o banco de dados dos Cadastros Padrões
-var objDados =
-    [
-        { Nome: "André De Oliveira", Email: "andre_1234@yahoo.com.br", Senha: "andre1234" },
-        { Nome: "Breno Rosa", Email: "breno_1234@yahoo.com.br", Senha: "breno1234" },
-        { Nome: "Gabriel Victor", Email: "gabriel_1234@yahoo.com.br", Senha: "gabriel1234" },
-        { Nome: "João Gabriel", Email: "joao_1234@yahoo.com.br", Senha: "joao1234" },
-        { Nome: "Admin", Email: "admin@admin.com", Senha: "admin" }
-    ]
+
 
 //Função de Salvar os Dados
 function salvaDados(dados) {
