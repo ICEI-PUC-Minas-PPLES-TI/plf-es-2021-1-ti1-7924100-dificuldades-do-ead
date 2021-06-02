f3e6346c79533538b0effb6be6970664bf0ee249
Gabriel Victor <gabriel.victorc13@gmail.com>
2021-06-01 21:44:43 -0300

    repensada a estrutura do elemento main da pagina home

--- Codigo/assets/data/DADOS.js
+++ Codigo/assets/data/DADOS.js
@@ -1,9 +1,10 @@
 var db = {
+    usuarioLogadoAtualmente:0,
     ultimoId:5,
     usuarios: [
-        { id: 1, Nome: "André De Oliveira", Email: "andre_1234@yahoo.com.br", Senha: "andre1234" },
-        { id: 2, Nome: "Breno Rosa", Email: "breno_1234@yahoo.com.br", Senha: "breno1234" },
-        { id: 3, Nome: "Gabriel Victor", Email: "gabriel@yahoo.com.br", Senha: "gabriel1234" },
+        { id: 1, Nome: "Gabriel Victor", Email: "gabriel@yahoo.com.br", Senha: "gabriel1234" },
+        { id: 2, Nome: "André De Oliveira", Email: "andre_1234@yahoo.com.br", Senha: "andre1234" },
+        { id: 3, Nome: "Breno Rosa", Email: "breno_1234@yahoo.com.br", Senha: "breno1234" },
         { id: 4, Nome: "João Gabriel", Email: "joao_1234@yahoo.com.br", Senha: "joao1234" },
         { id: 5, Nome: "Admin", Email: "admin@admin.com", Senha: "admin" }
     ],
@@ -11,6 +12,23 @@ var db = {
         { id: 1, listas:[
             {
                 lista_id:1, 
+                lista_nome:'Lista exemplo', 
+                lista_cor:'#ff0228',
+                lista_itens:[
+                    {
+                        titulo:'Quiz da palestra',
+                        descricao:'Para segunda feira um quiz sobre a palestra de Ana Regina',
+                        data:'07/06/2021'
+                    },
+                    {
+                        titulo:'Teste de software',
+                        descricao:'Fazer teste de um software',
+                        data:'10/06/2021'
+                    }
+                ]
+            },
+            {
+                lista_id:2, 
                 lista_nome:'Engenharia de software', 
                 lista_cor:'#ff0228',
                 lista_itens:[
@@ -27,7 +45,7 @@ var db = {
                 ]
             }
         ]},
-        
+
         { id: 2,},
         { id: 3,},
         { id: 4,},
