c5a1b52df60329b4ad37489a8edc40037fa915c2
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-06-03 18:31:15 -0300

    Correção de bugs na recuperação de senha

--- Codigo/assets/pages/recuperar-senha/rec-senha.js
+++ Codigo/assets/pages/recuperar-senha/rec-senha.js
@@ -3,10 +3,11 @@ function salvaDados(dados) {
     localStorage.setItem('db', JSON.stringify(dados));
 }
 
-//[---------------RECUPERAR SENHA-------------]
 var preDb = localStorage.getItem('db'); // Recuperando o banco de dados inteiro do localStorage
 var db = JSON.parse(preDb) // Tornando os dados recuperados em um objeto
+    //[---------------RECUPERAR SENHA-------------]
 function trocarSenha() {
+
 
 
 
@@ -14,15 +15,18 @@ function trocarSenha() {
     var Resposta = document.querySelector("input#Resposta").value;
     //var Senha = document.querySelector("input#Senha").value;
     var strSenha = document.querySelector('input#campoSenha').value;
-
-
+    console.log(Email);
+    console.log(Resposta)
+    console.log(db)
+    console.log(db.usuarios);
     for (let i = 0; i < db.usuarios.length; i++) { //ler usuario do banco de dados
         var usuario = db.usuarios[i];
+        console.log(usuario)
 
         if (Email == usuario.Email && Resposta == usuario.Resposta) { //identificar se a resposta está correta
             console.log(Email);
             console.log(Resposta);
-            db.usuario[i].Senha = strSenha
+            db.usuarios[i].Senha = strSenha
 
 
             window.alert("Senha alterada com sucesso");
