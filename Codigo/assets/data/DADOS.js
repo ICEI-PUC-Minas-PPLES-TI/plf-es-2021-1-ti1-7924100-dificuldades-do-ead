f489fba8f10514da961bd9951e22a5983268d51e
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-06-03 18:08:38 -0300

    Solucionando bugs de recuperação de senha

--- Codigo/assets/data/DADOS.js
+++ Codigo/assets/data/DADOS.js
@@ -1,54 +1,53 @@
 var db = {
-    usuarioLogadoAtualmente:0,
-    indexDaListaDousuario:0,
-    ultimoId:5,
+    usuarioLogadoAtualmente: 0,
+    indexDaListaDousuario: 0,
+    ultimoId: 5,
     usuarios: [
-        { id: 1, Nome: "Gabriel Victor", Email: "gabriel@yahoo.com.br", Senha: "gabriel1234" },
-        { id: 2, Nome: "André De Oliveira", Email: "andre_1234@yahoo.com.br", Senha: "andre1234" },
-        { id: 3, Nome: "Breno Rosa", Email: "breno_1234@yahoo.com.br", Senha: "breno1234" },
-        { id: 4, Nome: "João Gabriel", Email: "joao_1234@yahoo.com.br", Senha: "joao1234" },
-        { id: 5, Nome: "Admin", Email: "admin@admin.com", Senha: "admin" }
+        { id: 1, Nome: "Gabriel Victor", Email: "gabriel@yahoo.com.br", Senha: "gabriel1234", Resposta: 'resp' },
+        { id: 2, Nome: "André De Oliveira", Email: "andre_1234@yahoo.com.br", Senha: "andre1234", Resposta: 'resp' },
+        { id: 3, Nome: "Breno Rosa", Email: "breno_1234@yahoo.com.br", Senha: "breno1234", Resposta: 'resp' },
+        { id: 4, Nome: "João Gabriel", Email: "joao_1234@yahoo.com.br", Senha: "joao1234", Resposta: 'resp' },
+        { id: 5, Nome: "Admin", Email: "admin@admin.com", Senha: "admin", Resposta: 'resp' }
     ],
-    listasUsuarios:[
-        { id: 1, listas:[
-            {
-                lista_id:1, 
-                lista_nome:'Lista exemplo', 
-                lista_cor:'#FF006E',
-                lista_itens:[
-                    {
-                        titulo:'Quiz da palestra',
-                        descricao:'Para segunda feira um quiz sobre a palestra de Ana Regina',
-                        data:'07/06/2021'
-                    },
-                    {
-                        titulo:'Teste de software',
-                        descricao:'Fazer teste de um software',
-                        data:'10/06/2021'
-                    }
-                ]
-            },
-            {
-                lista_id:2, 
-                lista_nome:'Engenharia de software', 
-                lista_cor:'#3A86FF',
-                lista_itens:[
-                    {
-                        titulo:'Quiz da palestra',
-                        descricao:'Para segunda feira um quiz sobre a palestra de Ana Regina',
-                        data:'07/06/2021'
-                    },
-                    {
-                        titulo:'Teste de software',
-                        descricao:'Fazer teste de um software',
-                        data:'10/06/2021'
-                    }
-                ]
-            }
-        ]},
-        { id: 2,},
-        { id: 3,},
-        { id: 4,},
-        { id: 5,},
+    listasUsuarios: [{
+            id: 1,
+            listas: [{
+                    lista_id: 1,
+                    lista_nome: 'Lista exemplo',
+                    lista_cor: '#FF006E',
+                    lista_itens: [{
+                            titulo: 'Quiz da palestra',
+                            descricao: 'Para segunda feira um quiz sobre a palestra de Ana Regina',
+                            data: '07/06/2021'
+                        },
+                        {
+                            titulo: 'Teste de software',
+                            descricao: 'Fazer teste de um software',
+                            data: '10/06/2021'
+                        }
+                    ]
+                },
+                {
+                    lista_id: 2,
+                    lista_nome: 'Engenharia de software',
+                    lista_cor: '#3A86FF',
+                    lista_itens: [{
+                            titulo: 'Quiz da palestra',
+                            descricao: 'Para segunda feira um quiz sobre a palestra de Ana Regina',
+                            data: '07/06/2021'
+                        },
+                        {
+                            titulo: 'Teste de software',
+                            descricao: 'Fazer teste de um software',
+                            data: '10/06/2021'
+                        }
+                    ]
+                }
+            ]
+        },
+        { id: 2, },
+        { id: 3, },
+        { id: 4, },
+        { id: 5, },
     ]
 }
--- Codigo/assets/pages/recuperar-senha/rec-senha.js
+++ Codigo/assets/pages/recuperar-senha/rec-senha.js
@@ -1,26 +1,37 @@
+//Função de Salvar os Dados
+function salvaDados(dados) {
+    localStorage.setItem('db', JSON.stringify(dados));
+}
+
+//[---------------RECUPERAR SENHA-------------]
+
 function trocarSenha() {
 
+    var preDb = localStorage.getItem('db'); // Recuperando o banco de dados inteiro do localStorage
+    var db = JSON.parse(preDb) // Tornando os dados recuperados em um objeto
 
     var Email = document.querySelector("input#Email").value;
     var Resposta = document.querySelector("input#Resposta").value;
     //var Senha = document.querySelector("input#Senha").value;
+    var strSenha = document.querySelector('input#campoSenha').value;
 
 
-    for (let i = 0; i < db.usuarios.length; i++) {//ler usuario do banco de dados
+    for (let i = 0; i < db.usuarios.length; i++) { //ler usuario do banco de dados
         var usuario = db.usuarios[i];
 
-        if (Email == usuario.Email && Resposta == usuario.Resposta) {//identificar se a resposta está correta
-
-            let strSenha = document.getElementById('campoSenha').value;
-
+        if (Email == usuario.Email && Resposta == usuario.Resposta) { //identificar se a resposta está correta
+            console.log(Email);
+            console.log(Resposta);
             db.usuario[i].Senha = strSenha
 
 
             window.alert("Senha alterada com sucesso");
 
-            //window.location.href = "/plf-es-2021-1-ti1-7924100-dificuldades-do-ead/Codigo/login.html";//colocar link para pagina de login
+            //window.location.href = "#"; //colocar link para pagina de login
 
             salvaDados(db)
         }
     }
 }
+
+//[------------------------FIM RECUPERAR SENHA-----------]
