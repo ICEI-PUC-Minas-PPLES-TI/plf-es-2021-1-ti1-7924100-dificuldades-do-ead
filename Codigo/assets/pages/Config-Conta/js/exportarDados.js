9e6d17912c54f2457a9778d56bb3403106f211e3
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-06-29 19:10:37 -0300

    Função para exportar em XML Adicionada

--- Codigo/assets/pages/Config-Conta/js/exportarDados.js
+++ Codigo/assets/pages/Config-Conta/js/exportarDados.js
@@ -48,7 +48,7 @@ function exportarDados() {
             break;
     }
 };
-function exportarJSON(){
+function exportarJSON() {
     console.log('Exportando em JSON...');
     let dadosUsuario = localizarUsuario();
     let db = JSON.parse(localStorage.getItem('db'));
@@ -60,21 +60,21 @@ function exportarJSON(){
     downloadDados("Dados.json", conteudoJSON);
 }
 
-function exportarTXT(){
-    console.log('Exportando...');
+function exportarTXT() {
+    console.log('Exportando em TXT...');
     let dadosUsuario = localizarUsuario();
     let db = JSON.parse(localStorage.getItem('db'));
     let conteudoCabecalho = `${db.usuarios[dadosUsuario.indexDoUsuario].Nome}\n==========LISTAS==========\n`;
     let conteudo = [conteudoCabecalho];
     let listasDoUsuario = db.listasUsuarios[dadosUsuario.indexDoConjuntoDeListas].listas
-    for(let i = 0 ; i <  listasDoUsuario.length; i++){
+    for (let i = 0; i < listasDoUsuario.length; i++) {
         let estruturaLista = `\nTitulo da Lista: ${listasDoUsuario[i].lista_nome}\nCor da lista: ${listasDoUsuario[i].lista_cor}\nItens da lista:\n`
         let listaItens = [];
-        for(let j = 0 ; j<listasDoUsuario[i].lista_itens.length ; j++){
-            if(listasDoUsuario[i].lista_itens[j].is_checked){
+        for (let j = 0; j < listasDoUsuario[i].lista_itens.length; j++) {
+            if (listasDoUsuario[i].lista_itens[j].is_checked) {
                 let listaEstruturaItem = `\nTarefa: [*] ${listasDoUsuario[i].lista_itens[j].titulo}\ndescricao: ${listasDoUsuario[i].lista_itens[j].descricao}\ndata: ${listasDoUsuario[i].lista_itens[j].data}`
                 listaItens.push(listaEstruturaItem);
-            }else{
+            } else {
                 let listaEstruturaItem = `\nTarefa: [ ] ${listasDoUsuario[i].lista_itens[j].titulo}\ndescricao: ${listasDoUsuario[i].lista_itens[j].descricao}\ndata: ${listasDoUsuario[i].lista_itens[j].data}`
                 listaItens.push(listaEstruturaItem);
             }
@@ -85,48 +85,112 @@ function exportarTXT(){
     }
     downloadDados("dados.txt", conteudo)
 }
-function exportarPDF(){
+function exportarPDF() {
+
+    console.log('Exportando em PDF...');
+
+    let dadosUsuario = localizarUsuario();
+    let db = JSON.parse(localStorage.getItem('db'));
+    let conteudoCabecalho = `${db.usuarios[dadosUsuario.indexDoUsuario].Nome}\n==========LISTAS==========\n`;
+    let conteudo = [conteudoCabecalho];
+    let listasDoUsuario = db.listasUsuarios[dadosUsuario.indexDoConjuntoDeListas].listas
+    for (let i = 0; i < listasDoUsuario.length; i++) {
+        let estruturaLista = `\nTitulo da Lista: ${listasDoUsuario[i].lista_nome}\nCor da lista: ${listasDoUsuario[i].lista_cor}\nItens da lista:\n`
+        let listaItens = [];
+        for (let j = 0; j < listasDoUsuario[i].lista_itens.length; j++) {
+            if (listasDoUsuario[i].lista_itens[j].is_checked) {
+                let listaEstruturaItem = `\nTarefa: [*] ${listasDoUsuario[i].lista_itens[j].titulo}\ndescricao: ${listasDoUsuario[i].lista_itens[j].descricao}\ndata: ${listasDoUsuario[i].lista_itens[j].data}\n`
+                listaItens.push(listaEstruturaItem);
+            } else {
+                let listaEstruturaItem = `\nTarefa: [ ] ${listasDoUsuario[i].lista_itens[j].titulo}\ndescricao: ${listasDoUsuario[i].lista_itens[j].descricao}\ndata: ${listasDoUsuario[i].lista_itens[j].data}\n`
+                listaItens.push(listaEstruturaItem);
+            }
+        }
+        listaItens.join(" ")
+        let preConteudo = `${estruturaLista}\n${listaItens}\n-------------------\n`;
+        conteudo.push(preConteudo);
+    }
+
+    conteudo.join(' ');
+    conteudo = conteudo.toString();
+    const doc = new jsPDF();
+    lines = doc.splitTextToSize(conteudo, 150)
+    doc.text(10, 10, lines)
+    doc.save("dados.pdf");
+
+}
+function exportarXML() {
+    console.log('Exportando em XML...');
+    let dadosUsuario = localizarUsuario();
+    let db = JSON.parse(localStorage.getItem('db'));
+    let conteudo = {
+        nome: db.usuarios[dadosUsuario.indexDoUsuario].Nome,
+        listas: db.listasUsuarios[dadosUsuario.indexDoConjuntoDeListas].listas,
+    }
+    let conteudoXML = converterParaXML(conteudo)
+    downloadDados("dados.xml", conteudoXML);
+}
+function exportarCSV() {
+    console.log('Exportando em CSV...');
+    let dadosUsuario = localizarUsuario();
+    let db = JSON.parse(localStorage.getItem('db'));
+    let conteudo = [];
+    let conteudoJSON = JSON.stringify(conteudo);
+
+
+}
+function exportarMD() {
     console.log('Exportando...');
 
 }
-function exportarXML(){
-    console.log('Exportando...');
 
-}
-function exportarCSV(){
-    console.log('Exportando...');
-
-}
-function exportarMD(){
-    console.log('Exportando...');
-
+function downloadDados(nomeArquivo, conteudo) {
+    var blob = new Blob([conteudo], {
+        type: "sh/plain;charset=utf-8"
+    });
+    console.log(conteudo)
+    saveAs(blob, nomeArquivo);
 }
 
-function downloadDados(nomeArquivo, conteudo){
-	var blob = new Blob([conteudo], {
-		type: "sh/plain;charset=utf-8"
-	});
-	console.log(conteudo)
-	saveAs(blob, nomeArquivo);
+function converterParaXML(obj) {
+    var xml = '';
+    for (var prop in obj) {
+        xml += obj[prop] instanceof Array ? '' : "<" + prop + ">";
+        if (obj[prop] instanceof Array) {
+            for (var array in obj[prop]) {
+                xml += "<" + prop + ">";
+                xml += converterParaXML(new Object(obj[prop][array]));
+                xml += "</" + prop + ">";
+            }
+        } else if (typeof obj[prop] == "object") {
+            xml += converterParaXML(new Object(obj[prop]));
+        } else {
+            xml += obj[prop];
+        }
+        xml += obj[prop] instanceof Array ? '' : "</" + prop + ">";
+    }
+    var xml = xml.replace(/<\/?[0-9]{1,}>/g, '');
+    xml = `<dados>${xml}</dados>`;
+    return xml;
 }
 
-function localizarUsuario(){
+function localizarUsuario() {
     let db = JSON.parse(localStorage.getItem('db'))
     let usuario_id = db.usuarioLogadoAtualmente;
     var dadosUsuario = {
         id: 1,
-        indexDoUsuario:0,
+        indexDoUsuario: 0,
         indexDoConjuntoDeListas: 0,
     }
-    for(let i = 0 ; i < db.listasUsuarios.length ; i++){
-        if(db.listasUsuarios[i].id == usuario_id){
+    for (let i = 0; i < db.listasUsuarios.length; i++) {
+        if (db.listasUsuarios[i].id == usuario_id) {
             dadosUsuario.id = usuario_id;
             dadosUsuario.indexDoConjuntoDeListas = i;
             break;
         }
     }
-    for(let j = 0; j < db.usuarios.length ; j++){
-        if(db.usuarios[j].id == usuario_id){
+    for (let j = 0; j < db.usuarios.length; j++) {
+        if (db.usuarios[j].id == usuario_id) {
             dadosUsuario.indexDoUsuario = j;
         }
     }
