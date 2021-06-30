20319b57a876429832a7c3466cb3e0375ac7c15f
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-06-30 00:31:29 -0300

    Deploy versão 0.9

--- assets/js/gerador.js
+++ assets/js/gerador.js
@@ -127,19 +127,15 @@ function SHA256(s) {
 function byteToHex(byte) {
     return ('0' + byte.toString(16)).slice(-2);
 }
-
-// str generateId(int len);
-//   len - must be an even number (default: 40)
 function generateId(len = 40) {
     var arr = new Uint8Array(len / 2);
     window.crypto.getRandomValues(arr);
     return Array.from(arr, byteToHex).join("");
 }
-
-function generateListId(){
+function generateListId() {
     return Math.floor(Math.random() * Date.now());
 }
-function generateItemId(){
+function generateItemId() {
     return Math.floor((Math.random() * 99999999) + 10000001);
-  }
+}
 
